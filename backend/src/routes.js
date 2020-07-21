ca4fb9c2e8e68741316f1118991590b394469c60
natalia-sa <natalia.andre@ccc.ufcg.edu.br>
2020-07-21 00:52:13 -0300

    corrigindo erros

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,42 +2,40 @@ const express = require('express');
 const multer = require('multer');
 const uploadConfig = require('./config/upload')
 
-// const SessionController = require('./controllers/SessionController');
-// const SpotController = require('./controllers/SpotController');
-// const DashBoardController = require('./controllers/DashBoardController');
-// const BookingController = require('./controllers/BookingController');
-// const ApprovalController = require('./controllers/ApprovalController');
-// const RejectionController = require('./controllers/RejectionController');
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashBoardController = require('./controllers/DashBoardController');
+const BookingController = require('./controllers/BookingController');
+const ApprovalController = require('./controllers/ApprovalController');
+const RejectionController = require('./controllers/RejectionController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
 
-// routes.post('/sessions', function(req,res) {
-//     SessionController.store;
-// });
+routes.post('/sessions', function(req,res) {
+    SessionController.store;
+});
 
-// routes.get('/dashboard', DashBoardController.show);
+routes.get('/dashboard', DashBoardController.show);
 
-// routes.get('/spots', SpotController.index);
+routes.get('/spots', SpotController.index);
 
-// routes.post('/spots',upload.single('thumbnail'),function(req,res){
-//     SpotController.store;
-// } )
+routes.post('/spots',upload.single('thumbnail'),function(req,res){
+    SpotController.store;
+} )
 
-// routes.post('/spots/:spot_id/bookings', function(req, res) {
-//     BookingController.store;
-// });
+routes.post('/spots/:spot_id/bookings', function(req, res) {
+    BookingController.store;
+});
 
-// routes.post('./bookings/:booking_id/approvals', function(req,res) {
-//     ApprovalController.store;
-// });
+routes.post('./bookings/:booking_id/approvals', function(req,res) {
+    ApprovalController.store;
+});
 
-// routes.post('./bookings/:booking_id/rejections', function(req, res) {
-//     RejectionController.store
-// });
-// routes.get('/', function(req,res) {
-//     res.send("hello world");
-// });
+routes.post('./bookings/:booking_id/rejections', function(req, res) {
+    RejectionController.store
+});
+
 
 module.exports = routes;
---
+++ backend/src/server.js
@@ -0,0 +1,54 @@
+// GET, POST, PUT, DELETE
+// req.query = acessar query params(para filtrar) na rota get
+// req.params = acessar route params (para edição, delete)
+// req.body = acessar corpo da requisição (criação, edição)
+
+const express = require('express');
+const mongoose = require('mongoose')
+const cors = require('cors');
+const routes = require('./routes');
+const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
+
+// cria a aplicação com a função express
+const app = express();
+const server = http.Server(app);
+// para o server ouvir o protocolo webSocket
+// precisamos da variavel io para anotar os usuarios da aplicação
+const io = socketio(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    
+    // a mensagem se chama hello e seu conteudo é world
+    const { user_id } = socket.handshake.query;
+    // relacionando o id do usuario a seu id de conexao
+    connectedUsers[user_id] = socket.id;
+});
+// adicionar uma funcionalidade em toda rota
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
+
+mongoose.connect('mongodb+srv://natalia:novasenha2@omnistack-oq54w.mongodb.net/semana09?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+app.use(cors());
+
+// sinalizando para o express que sera utilizado o formato json
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..','uploads')));
+
+// usando as rotas importadas
+app.get('/', function(req, res) {
+    res.send('hello world');
+  });
+app.use(routes);
+server.listen(process.env.PORT || 3000);
