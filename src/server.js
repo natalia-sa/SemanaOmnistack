d51c978b9d50d53bf47277ec37dd0ea2302f3485
natalia-sa <natalia.andre@ccc.ufcg.edu.br>
2020-07-23 01:36:47 -0300

    testando cors

--- src/server.js
+++ src/server.js
@@ -14,11 +14,36 @@ const http = require('http');
 // cria a aplicação com a função express
 const app = express();
 const server = http.Server(app);
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    app.use(cors());
-    next();
-});
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
+  
+  app.use(cors(corsOpts));
+(function() {
+    var cors_api_host = 'cors-anywhere.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+})();
 // para o server ouvir o protocolo webSocket
 // precisamos da variavel io para anotar os usuarios da aplicação
 const io = socketio(server);
